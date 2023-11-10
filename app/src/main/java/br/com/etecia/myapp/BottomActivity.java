5e226a0c3fc1f46fcbee2f3d976bdbcb1cce2b3e
joaoguilherme <joaoguilhermehig@gmail.com>
2023-11-09 21:42:55 -0300

    menu pronto bottom

--- app/src/main/java/br/com/etecia/myapp/BottomActivity.java
+++ app/src/main/java/br/com/etecia/myapp/BottomActivity.java
@@ -4,11 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class MenuBottomActivity extends AppCompatActivity {
+public class BottomActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.menu_bottom_layout);
+        setContentView(R.layout.bottom_bar_layout);
     }
 }
