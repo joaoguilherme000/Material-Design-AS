ff85fb84f65124e8f8992b17434a544ac7e548e4
joaoguilherme <joaoguilhermehig@gmail.com>
2023-11-23 22:14:20 -0300

    coisa loca

--- app/src/main/java/br/com/etecia/myapp/MainActivity.java
+++ app/src/main/java/br/com/etecia/myapp/MainActivity.java
@@ -1,6 +1,7 @@
 package br.com.etecia.myapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
@@ -14,7 +15,11 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         idRecyclerViewFilmes = findViewById(R.id.idRecyclerViewFilmes);
+
+        idRecyclerViewFilmes.setLayoutManager(new GridLayoutManager(getApplicationContext(), 2));
+
+        idRecyclerViewFilmes.setHasFixedSize(true);
+
+        idRecyclerViewFilmes.setAdapter(idRecyclerViewFilmes.getAdapter());
     }
-
-
 }
