be4339149d780c160da058d73b572abcb0a4cba2
joaoguilherme <joaoguilhermehig@gmail.com>
2024-02-27 21:50:41 -0300

    card feito

---
+++ app/src/main/java/br/com/etecia/myapp/Lista_Produtos_Activity.java
@@ -0,0 +1,14 @@
+package br.com.etecia.myapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Lista_Produtos_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.lista_produtos_layout);
+    }
+}
