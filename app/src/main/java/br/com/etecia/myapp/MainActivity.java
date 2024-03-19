2c66088b8ac271627ccde33f5e699f5e4f790e12
joaoguilherme000 <joaoguilhermehig@gmail.com>
2024-03-19 20:15:13 -0300

    mudando o estilo

--- app/src/main/java/br/com/etecia/myapp/MainActivity.java
+++ app/src/main/java/br/com/etecia/myapp/MainActivity.java
@@ -29,34 +29,20 @@ public class MainActivity extends AppCompatActivity {
         });
 
         lstLivros = new ArrayList<>();
-        lstLivros.add(new Livros("A realidade de Madhu", "Ficção Científica", " Madhu é abduzida por uma nave intergaláctica", R.drawable.a_realidade));
-        lstLivros.add(new Livros("O sol é para todos", "Ficção", "Um dos maiores clássicos da literatura mundial", R.drawable.sol_para_todos));
-        lstLivros.add(new Livros("Eu e Esse Meu Coração", "Auto ajuda", "Eu e Esse Meu Coração", R.drawable.coracao));
-        lstLivros.add(new Livros("Dieta", "Alimentação", "Este não é mais um livro de dieta: O novo e libertador estilo de vida alimentar para saúde e boa", R.drawable.dieta));
-        lstLivros.add(new Livros("O dilema", "Auto ajuda", "O dilema do porco espinho", R.drawable.dilema));
-        lstLivros.add(new Livros("Como fazer Amigos", "Auto ajuda", "Como fazer amigos", R.drawable.amigos));
-        lstLivros.add(new Livros("The Vegitarian ", "Romance", "The Vegitarian Life", R.drawable.thevigitarian));
-        lstLivros.add(new Livros("The Wild Robot", "Categoria do livro", "Descrição do livro", R.drawable.thewildrobot));
-        lstLivros.add(new Livros("The Martian", "Categoria do livro", "Descrição do livro", R.drawable.themartian));
-        lstLivros.add(new Livros("Privacy", "Categoria do livro", "Descrição do livro", R.drawable.privacy));
-        lstLivros.add(new Livros("Maria Semples", "Categoria do livro", "Descrição do livro", R.drawable.mariasemples));
-        lstLivros.add(new Livros("Hedied With", "Categoria do livro", "Descrição do livro", R.drawable.hediedwith));
-        lstLivros.add(new Livros("O dilema", "Auto ajuda", "O dilema do porco espinho", R.drawable.dilema));
-        lstLivros.add(new Livros("Iracema", "Clássico", "Uma das histórias de amor mais aclamadas da literatura brasileira", R.drawable.a_realidade));
-        lstLivros.add(new Livros("O Homem Que Sabia Javanês", "Clássico", "Uma chamada que muitos deixariam passar, Castelo enxergou uma oportunidade única.", R.drawable.ohomequesabia));
-        lstLivros.add(new Livros("Como fazer Amigos", "Auto ajuda", "Como fazer amigos", R.drawable.amigos));
-        lstLivros.add(new Livros("The Vegitarian ", "Romance", "The Vegitarian Life", R.drawable.thevigitarian));
-        lstLivros.add(new Livros("The Wild Robot", "Categoria do livro", "Descrição do livro", R.drawable.thewildrobot));
-        lstLivros.add(new Livros("The Martian", "Categoria do livro", "Descrição do livro", R.drawable.themartian));
-        lstLivros.add(new Livros("Privacy", "Categoria do livro", "Descrição do livro", R.drawable.privacy));
-        lstLivros.add(new Livros("Maria Semples", "Categoria do livro", "Descrição do livro", R.drawable.mariasemples));
-        lstLivros.add(new Livros("Hedied With", "Categoria do livro", "Descrição do livro", R.drawable.hediedwith));
-        lstLivros.add(new Livros("O poder do agora", "Auto Ajuda", "Um guia para a iluminação espiritual ", R.drawable.poder_agora));
-        lstLivros.add(new Livros("Pai rico, pai pobre para jovens", "Administração e Negócios", "o que a escola não ensina sobre dinheiro", R.drawable.pai));
+        lstLivros.add(new Livros("john wick 4", "ação", " john wick sai matando todo mundo", R.drawable.filmes1));
+        lstLivros.add(new Livros("The batman", "Ficção", "Um dos maiores clássicos da literatura mundial", R.drawable.filmes2));
+        lstLivros.add(new Livros("Homem aranha 3", "Auto ajuda", "Eu e Esse Meu Coração", R.drawable.filmes3));
+        lstLivros.add(new Livros("The grinch", "Alimentação", "Este não é mais um livro de dieta: O novo e libertador estilo de vida alimentar para saúde e boa", R.drawable.filmes4));
+        lstLivros.add(new Livros("Mario bros", "Auto ajuda", "O dilema do porco espinho", R.drawable.filmes6));
+        lstLivros.add(new Livros("Um estranho no ninho", "Auto ajuda", "Como fazer amigos", R.drawable.filmes7));
+        lstLivros.add(new Livros("Pinnochio", "Romance", "The Vegitarian Life", R.drawable.filmes5));
+        lstLivros.add(new Livros("Homem aranha no aranha verso", "Categoria do livro", "Descrição do livro", R.drawable.filmes8));
+        lstLivros.add(new Livros("o nevoeiro", "Categoria do livro", "Descrição do livro", R.drawable.filmes9));
+        lstLivros.add(new Livros("Enigma de outro mundo", "Categoria do livro", "Descrição do livro", R.drawable.filmes10));
 
         recyclerViewLivros = findViewById(R.id.idRecyclerLivros);
 
-        recyclerViewLivros.setLayoutManager(new GridLayoutManager(getApplicationContext(), 3));
+        recyclerViewLivros.setLayoutManager(new GridLayoutManager(getApplicationContext(),2));
 
         ListaAdapter adapter = new ListaAdapter(getApplicationContext(), lstLivros);
 
